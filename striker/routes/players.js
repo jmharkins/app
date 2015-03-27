f6b349640e309676fed817b9644b7b7e00fec5bf
jmh-fic <johannesh@first-insight.com>
2015-03-27 13:55:37 -0700

    Player API route working
    Player API route returns data by ID

--- striker/routes/players.js
+++ striker/routes/players.js
@@ -2,15 +2,35 @@ var express = require('express');
 var MongoClient = require('mongodb').MongoClient
 var router = express.Router();
 
+var filterer = function(type, arr, id) {
+	var f;
+	if (type === "team") {
+		f = arr.filter(function(d) { return d.team_id === id && d.type !== "blocked" && d.is_own !== "yes"  && d.is_penalty !== 1 })
+	} else if (type === "player") {
+		f = arr.filter(function(d) {return d.player_id === id && d.type !== "blocked" && d.is_own !== "yes"  && d.is_penalty !== 1})
+	}
+	return f
+}
+
 /* GET player data by ID. */
 router.get('/players/:id', function(req, res, next) {
-  var tid = req.params.id;
+  var qid = req.params.id;
   var dbq = MongoClient.connect("mongodb://localhost:27017/epldb", function(err, db) {
 	if(err) { return next(err); }
 		var gcollect = db.collection('games2');
-		var qresult = gcollect.find({'game_id': tid }, {fields: {team0: 1, team1: 1, _id: 0}}).nextObject(function(err,doc) {
-			res.send(JSON.stringify(doc));
-			db.close();
+		var outarray = [];
+		var arsshots = gcollect.find({'players.id': qid},{fields: {shots: 1, _id: 0}})
+		  .each(function(err, item){ 
+			// If the item is null then the cursor is exhausted/empty and closed
+			if(item == null) {
+				// send back JSON
+				res.send(JSON.stringify(outarray));
+				db.close();
+			} else {
+				var shots = item.shots;
+				var fshots = filterer("player",shots,qid);
+				outarray = outarray.concat(fshots)
+			}
 		});
 	});
 });
